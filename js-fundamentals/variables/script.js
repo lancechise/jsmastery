08d2cf192fa00df6176b64070cb44316934fb3b1
lancechise <lancechise@icloud.com>
2023-01-25 14:49:33 -0500

    js mastery

--- js-fundamentals/variables/script.js
+++ js-fundamentals/variables/script.js
@@ -1,1 +1,66 @@
 "use strict";
+
+//Example 1
+// console.log("welcome to variables");
+
+//example 2: Declaring Variable
+// var variableName = "Hello, World";
+// console.log(variableName);
+
+//Example 3:let Can be reassigned
+// let variableName = "Lance";
+// console.log(variableName);
+
+//Example 4:const Cannot be reassigned
+// const variableName2 = "Watson";
+// console.log(variableName2);
+
+//Example 5: Datatypes String
+
+// const exampleString = "Hello, World";
+// console.log(exampleString);
+
+//Example 6: Strings Single And Double Quotes
+// const singleQuotes = "Hello, World";
+// console.log(singleQuotes);
+
+// const doubleQuotes = "Hello, World";
+// console.log(doubleQuotes);
+
+//Example 6:Backticks
+// const backticks = `Hello!!!`;
+// console.log(backticks);
+
+// const name = "Lance";
+// const backticks = `Hello, ${name}`;
+
+// console.log(backticks);
+
+//Example 7: Numbers
+// const wholeNumber = 5;
+// const decimalNumber = 0.5;
+
+// console.log(wholeNumber);
+// console.log(decimalNumber);
+// const result = wholeNumber - decimalNumber;
+// console.log(result);
+
+//Example 8: Booleans: Only two values.
+//True means yes, represented by 1
+//False means no, represented by 0
+const isCool = true;
+console.log(isCool);
+
+//Example 9: Null: Means nothing. Empty.
+let age = null;
+
+//Example 10: Objects
+const person = {
+  name: "lance",
+  age: 25,
+};
+
+//Dot Notation person.name
+console.log(person.name);
+//Example 11:
+//Example 12:
